4b62501d151cf6c3970907c545da9137a9879630
jhenessholm <144741835+jhenessholm@users.noreply.github.com>
2023-09-22 17:07:05 +0200

    C (sharp)
    Assignment

---
+++ MyAssignment/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+
+using System;
+
+public class UserInput
+{
+    public static string GetUserInput (string message)
+    {
+        Console.WriteLine(message);
+        string userText = Console.ReadLine().Trim();
+        while (string.IsNullOrEmpty(userText))
+        {
+            Console.WriteLine("Input cannot be empty. Please try again.");
+            Console.WriteLine(message);
+            userText = Console.ReadLine().Trim();
+        }
+        return userText;
+    }
+
+
+    public static string AddHashtag(string word)
+    {
+        Console.Write($"Enter some text for #{word}: ");
+        string userText = Console.ReadLine().Trim();
+        while (string.IsNullOrEmpty(userText))
+        {
+            Console.WriteLine("Input cannot be empty. Please try again.");
+            Console.Write($"Enter some text for #{word}: ");
+            userText = Console.ReadLine().Trim();
+        }
+        string result = $"#{userText}";
+        return result;
+    }
+    public static string FormatName(string firstName, string lastName)
+    {
+        string fullName = $"{lastName}, {firstName}";
+        return fullName;
+    }
+}
---
+++ MyAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Ask the user for their name and greet them
+        string userName = UserInput.GetUserInput("Please enter your name: ");
+        Console.WriteLine($"Hello, {userName}!");
+
+
+        // Ask the user for a word and add a hashtag
+        string word = UserInput.GetUserInput("Please enter a word:");
+        string textWithHashtag = UserInput.AddHashtag(word);
+        Console.WriteLine($"Result: {textWithHashtag}");
+
+
+        // Ask the user for a number
+        string number = UserInput.GetUserInput("Please enter a number:");
+        Console.WriteLine($"You entered: {number}");
+
+
+        // Ask the user for a number (alternative approach)
+        string userEnteredText = UserInput.GetUserInput("Please enter a number: ");
+        Console.WriteLine($"You entered: {userEnteredText}");
+
+
+
+        // Ask the user for first and last name and format it
+        string firstName = UserInput.GetUserInput("Enter the first name: ");
+        string lastName = UserInput.GetUserInput("Enter the last name:");
+        string formattedName = UserInput.FormatName(firstName, lastName);
+        Console.WriteLine($"Formatted Name: {formattedName}");
+
+
+
+    }
+}
